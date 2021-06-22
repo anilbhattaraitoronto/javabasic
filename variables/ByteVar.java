f4b20ff9361b4c5146807de391fca8d9342b8484
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 12:34:45 -0400

    Created byte variable and used it in class ByteVar

---
+++ variables/ByteVar.java
@@ -0,0 +1,7 @@
+public class ByteVar{
+    public static void main(String[] args){
+        //create Byte variable
+        Byte myAge = 51;
+        System.out.println("My Age fits in byte: " + myAge);
+    }
+}
