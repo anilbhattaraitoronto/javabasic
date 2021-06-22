e293dc89ef116b7e3b1dab03f992b3e953762e73
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:12:22 -0400

    Created joined variable and used it

---
+++ variables/JoinVar.java
@@ -0,0 +1,9 @@
+public class JoinVar{
+    public static void main(String [] args){
+        //Joining variables to make new variable
+        String firstName = "Anil";
+        String lastName = "Bhattarai";
+        String fullName = firstName + " " + lastName;
+        System.out.println("My full name is: " + fullName);
+    }
+}
