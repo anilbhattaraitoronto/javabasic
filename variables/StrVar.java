f3b5bf9a30bc125663e86897e8b108e0f9516044
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 12:09:50 -0400

    Added string variable file in variables folder to create and use a string variable

---
+++ variables/StrVar.java
@@ -0,0 +1,9 @@
+// Creating variables and using them
+
+public class StrVar {
+    public static void main(String [] args){
+        System.out.println("Creating string variable:");
+        String msg = "Hello, world!";
+        System.out.println(msg);
+    }
+}
