8eef5fe86619d74f3ae8f0da610033c24d2d2aec
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:08:44 -0400

    Added final variable and used it

---
+++ variables/FinalVar.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class FinalVar {
+    public static void main(String[] args){
+        //declaring final variable
+        final String myName = "Anil Bhattarai";
+        
+        System.out.println("My name is: " + myName);
+    }
+    
+}
