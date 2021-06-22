919c09603dd78cda99ee68a4fca622da6dabd2f5
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:21:59 -0400

    Created new branch datatypes and declared Byte type variable

---
+++ datatypes/ByteType.java
@@ -0,0 +1,8 @@
+public class ByteType{
+    public static void main(String[]args){
+        //Byte data type declaration
+        
+        Byte myByte = 5;
+        System.out.println("'" + myByte + "'" + " is byte data type.");
+    }
+}
