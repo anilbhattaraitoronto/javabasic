f5fa8787cf641a40db7512fd8594ac662eac7c48
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 11:08:16 -0400

    Comment content added

---
+++ Comments.java
@@ -0,0 +1,7 @@
+public class Comments {
+    public static void main(String [] args){
+        //Comments are interesting
+        System.out.println("Comments are interesting with double forward slashes");
+        /* They are even more interesting in multi lines */
+    }
+}
