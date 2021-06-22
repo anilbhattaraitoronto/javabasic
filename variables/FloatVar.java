3a937aea749e2a510eeb85d0a73cc6293427b370
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 15:50:29 -0400

    added detailed comment

--- variables/FloatVar.java
+++ variables/FloatVar.java
@@ -1,6 +1,8 @@
 public class FloatVar{
     public static void main(String[] args){
-        //declare a float variable
+        /*declare a float variable. We need to put 'f' at the 
+        end of the value.
+        */ 
         float myShare = 4.67f;
         System.out.println("My Share is: " + myShare);
     }
