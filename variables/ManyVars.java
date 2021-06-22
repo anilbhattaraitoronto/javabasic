e382903f162ca53231c8feca7ca9ac8b9bc42451
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:14:44 -0400

    Created many variables in one line

---
+++ variables/ManyVars.java
@@ -0,0 +1,7 @@
+public class ManyVars{
+    public static void main(String[] args){
+        //declare many variables
+        String firstName="Anil", lastName="Bhattarai", fullName=firstName + " " + lastName;
+        System.out.println("My fullname is: " + fullName);
+    }
+}
