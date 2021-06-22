ff3f4729a74f343ab2affa2cb1406df7b176e173
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 18:00:26 -0400

    Created and used float variable

---
+++ datatypes/FloatType.java
@@ -0,0 +1,7 @@
+public class FloatType {
+    public static void main(String[] args){
+        //declare a floating point type variable
+        float mySmallFraction = 1.34f;
+        System.out.println("My small fraction is: " + mySmallFraction);
+    }
+}
