4e02083bfe83fff59c151ed2901c2c6e22b9a7b1
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:52:28 -0400

    created and used int variable

---
+++ datatypes/IntType.java
@@ -0,0 +1,9 @@
+public class IntType{
+    public static void main(String[] args){
+        //create int type variable and use it
+
+        int oneLakh = 100000;
+
+        System.out.println("One lakh is: " + oneLakh);
+    }
+}
