e5f176b547664ba17125d3982120d97b1ea55ac1
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:55:38 -0400

    Created and used a long type variable

---
+++ datatypes/LongType.java
@@ -0,0 +1,10 @@
+public class LongType{
+    public static void main(String[] args){
+        //declare long type variable
+
+        long veryLongNum = 123000000000000L;
+        // use the long variable
+        System.out.println("An example of a very long number is: " + veryLongNum);
+
+    }
+}
