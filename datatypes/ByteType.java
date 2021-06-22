e37efd665211ae8ae1c464248651c4d63ac4a302
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 17:47:59 -0400

    created short type and used it

--- datatypes/ByteType.java
+++ datatypes/ByteType.java
@@ -1,8 +1,10 @@
+import javax.sound.sampled.AudioFileFormat.Type;
+
 public class ByteType{
     public static void main(String[]args){
         //Byte data type declaration
-        
-        Byte myByte = 5;
+
+        byte myByte = 5;
         System.out.println("'" + myByte + "'" + " is byte data type.");
     }
 }
---
+++ datatypes/ShortType.java
@@ -0,0 +1,7 @@
+public class ShortType{
+    public static void main(String [] args){
+        //declare, assign, and use a variable of short type
+        short totalSales = 2300;
+        System.out.println("Total sales was: " + totalSales);
+    }
+}
