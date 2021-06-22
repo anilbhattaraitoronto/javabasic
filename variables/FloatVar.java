e08078245427579bec8acf7fd2f483a1c9435e8e
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 12:40:05 -0400

    Created a floating point variable and used it

---
+++ variables/FloatVar.java
@@ -0,0 +1,7 @@
+public class FloatVar{
+    public static void main(String[] args){
+        //declare a float variable
+        float myShare = 4.67f;
+        System.out.println("My Share is: " + myShare);
+    }
+}
