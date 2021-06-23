7f443c7f3469e27a9b6109b9e155c8518f713c85
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 20:13:45 -0400

    Created and used String type variable

---
+++ datatypes/StrType.java
@@ -0,0 +1,10 @@
+package datatypes;
+
+public class StrType{
+    public static void main(String[] args){
+        //declare String variable
+
+        String myName = "Anil Bhattarai";
+        System.out.println("My name is: " + myName);
+    }
+}
