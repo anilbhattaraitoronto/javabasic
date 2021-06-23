8109d2e9ee19d9c6ce9f9e8bc3be10562737f422
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 20:11:38 -0400

    Created and used char type variable

---
+++ datatypes/CharType.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class CharType{
+    public static void main(String[]args){
+        //declare char type variable
+
+        char myGrade = 'A';
+        //use the variable
+
+        System.out.println("My grade in science is: " + myGrade);
+    }
+}
