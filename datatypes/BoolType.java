6b058943c16b87612d665161706d7e08d5317d5d
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-22 20:09:33 -0400

    Created and used a boolean variable

---
+++ datatypes/BoolType.java
@@ -0,0 +1,11 @@
+package datatypes;
+
+public class BoolType{
+    public static void main(String[] args){
+        //declare boolean variable
+
+        boolean isDeveloper = true;
+        System.out.println("Am I a developper?");
+        System.out.println("The answer is: " + isDeveloper);
+    }
+}
