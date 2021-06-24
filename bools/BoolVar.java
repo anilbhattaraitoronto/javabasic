b6fa2a933e7466c2fa2c69f057fa4de702cc996c
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-24 14:30:12 -0400

    Used different boolean expressions

---
+++ bools/BoolVar.java
@@ -0,0 +1,12 @@
+package bools;
+public class BoolVar{
+    public static void main(String[] args){
+        //Using boolean values
+        System.out.println(10 > 8);
+        System.out.println(10 >= 10);
+        System.out.println(10 <=9);
+        System.out.println(10 < 17);
+        System.out.println("a" == "a");
+        System.out.println("a" != "a");
+    }
+}
