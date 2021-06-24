30697104c6ce4b197eb588771c71fc2eee91a23a
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-24 11:52:39 -0400

    Used more math functions

--- maths/MathMax.java
+++ maths/MathMax.java
@@ -5,5 +5,7 @@ public class MathMax{
         //Using max function
         System.out.println(Math.max(35, 56));
         System.out.println(Math.min(24, 2));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-4.67));
     }
 }
