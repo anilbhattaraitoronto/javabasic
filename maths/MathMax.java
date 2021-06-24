8d3c266a7ac1de368e0b92155c641381e40f8f0a
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-24 11:48:30 -0400

    Used math min function

--- maths/MathMax.java
+++ maths/MathMax.java
@@ -4,5 +4,6 @@ public class MathMax{
     public static void main(String[] args){
         //Using max function
         System.out.println(Math.max(35, 56));
+        System.out.println(Math.min(24, 2));
     }
 }
