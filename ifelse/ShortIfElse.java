e9264fb33f60bfc2df11890c92d385cdca2b7e50
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-25 10:54:05 -0400

    Added ternery operation

---
+++ ifelse/ShortIfElse.java
@@ -0,0 +1,7 @@
+public class ShortIfElse{
+    public static void main (String[] args){
+        int myAge = 51;
+        String status = (myAge <50)? "Still young":"Now getting old!";
+        System.out.println(status);
+    }
+}
