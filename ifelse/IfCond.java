f07cd64c818ab08c0b9192b43df55136f5206262
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-25 10:48:26 -0400

    Added if else else if expressions

--- ifelse/IfCond.java
+++ ifelse/IfCond.java
@@ -2,6 +2,14 @@ package ifelse;
 
 public class IfCond{
     public static void main(String[] args){
-        
+        char firstLetter = 'a';
+        char secondLetter = 'b';
+        if (firstLetter == secondLetter){
+            System.out.println("First letter is equal to second letter");
+        }else if (firstLetter !=secondLetter){
+            System.out.println("They are not equal");
+        }else{
+            System.out.println("Something else");
+        }
     }
 }
